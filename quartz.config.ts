6d296d16bec67f5f6ec8ea7200f919f52459e685
miztee <mizumii1790@gmail.com>
2024-03-27 02:50:37 +0700

    Quartz sync: Mar 27, 2024, 2:50 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "MIz notes",
+    pageTitle: "this.world.guide",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
